7cce6d93afccaf2c5d457534befad19247940cf0
宝爷 <wyb10a10@163.com>
2019-11-13 22:27:17 +0800

    整理公告界面，添加meta文件

--- assets/Script/example/uiviews/UINotice.ts
+++ assets/Script/example/uiviews/UINotice.ts
@@ -1,31 +1,11 @@
-// Learn TypeScript:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+import { UIView } from "../../ui/UIView";
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
-
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {}
-
-    start () {
+export default class UINotice extends UIView {
+    
+    public init() {
 
     }
-
-    // update (dt) {}
 }

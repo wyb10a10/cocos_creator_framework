72c20d7baa0894b1fb5e8a9b4e8b364a4be46afe
宝爷 <wyb10a10@163.com>
2019-12-13 08:45:34 +0800

    CacheInfo增加number类型自增的useId

--- assets/Script/res/ResLoader.ts
+++ assets/Script/res/ResLoader.ts
@@ -15,7 +15,8 @@ export type CompletedCallback = (error: Error, resource: any) => void;
 // 引用和使用的结构体
 interface CacheInfo {
     refs: Set<string>,
-    uses: Set<string>
+    uses: Set<string>,
+    useId: number
 }
 
 // LoadRes方法的参数结构
@@ -40,7 +41,7 @@ if (!isChildClassOf) {
     isChildClassOf = cc["isChildClassOf"];
 }
 
-export class ResLoader {
+export default class ResLoader {
 
     private _resMap: Map<string, CacheInfo> = new Map<string, CacheInfo>();
 
@@ -127,7 +128,8 @@ export class ResLoader {
         if (!this._resMap.has(key)) {
             this._resMap.set(key, {
                 refs: new Set<string>(),
-                uses: new Set<string>()
+                uses: new Set<string>(),
+                useId: 0
             });
         }
         return this._resMap.get(key);

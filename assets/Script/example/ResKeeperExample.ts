db951a5f7f5aec62a5b3ec8f55f61e76b0507204
yongbaowang <yongbaowang@tencent.com>
2020-01-18 17:09:57 +0800

    ResUtil示例

---
+++ assets/Script/example/ResKeeperExample.ts
@@ -0,0 +1,44 @@
+import { resLoader } from "../res/ResLoader";
+import { ResUtil } from "../res/ResUtil";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class NetExample extends cc.Component {
+    @property(cc.Boolean)
+    resUtilMode = true;
+    @property(cc.Node)
+    attachNode: cc.Node = null;
+    @property(cc.Label)
+    dumpLabel: cc.Label = null;
+
+    onAdd() {
+        resLoader.loadRes("prefabDir/HelloWorld", cc.Prefab, (error: Error, prefab: cc.Prefab) => {
+            if (!error) {
+                let myNode = ResUtil.instantiate(prefab);
+                myNode.parent = this.attachNode;
+                myNode.setPosition((Math.random() * 500) - 250, myNode.position.y);
+                console.log(myNode.position);
+            }
+        });
+    }
+
+    onSub() {
+        if (this.attachNode.childrenCount > 0) {
+            this.attachNode.children[this.attachNode.childrenCount - 1].destroy();
+        }
+    }
+
+    onAssign() {
+        resLoader.releaseRes("prefabDir/HelloWorld", cc.Prefab);
+    }
+
+    onClean() {
+        this.attachNode.destroyAllChildren();
+    }
+
+    onDump() {
+        let Loader: any = cc.loader;
+        this.dumpLabel.string = `当前资源总数:${Object.keys(Loader._cache).length}`;
+    }
+}

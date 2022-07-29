60a126288c895d95515287dffe11252bcb60a4be
wyb10a10 <wyb10a10@163.com>
2022-07-29 22:13:10 +0800

    fix bug

--- assets/Script/sync/SyncUtil.ts
+++ assets/Script/sync/SyncUtil.ts
@@ -96,7 +96,7 @@ function makeObjectReplicatedMark(cls: any, option?: ObjectReplicatedOption): Re
 export function replicated(option?: ReplicatedOption) {
     // 真正的装饰器
     return (target: any, propertyKey: string, descriptor?: PropertyDescriptor) => {
-        return makePropertyReplicatedMark(target, propertyKey, descriptor, option);
+        return makePropertyReplicatedMark(target.constructor, propertyKey, descriptor, option);
     };
 }
 

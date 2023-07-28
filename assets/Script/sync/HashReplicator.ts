60cf62d8604c563da4de17106d814049360241ee
wyb10a10 <wyb10a10@163.com>
2023-07-28 20:36:28 +0800

    add hash replicator

--- assets/Script/sync/HashReplicator.ts
+++ assets/Script/sync/HashReplicator.ts
@@ -1,5 +1,6 @@
 import ReplicateMark from "./ReplicateMark";
-import { IReplicator, SimpleType, customRandom, isEqual } from "./SyncUtil";
+import { createReplicator } from "./ReplicatorFactory";
+import { Consturctor, IReplicator, SimpleType, customRandom, getConsturctor, isEqual } from "./SyncUtil";
 
 /**
  * Hash对象某个版本的数据
@@ -12,6 +13,7 @@ interface HashSimpleVersionInfo {
 enum HashActionType {
     Add,    // 添加：count, key1, value1, key2, value2, ...
     Delete, // 删除：count, key1, key2, ...
+    Update, // 更新，key1, diff1, key2, diff1, ...
     Clear,  // 清空
 }
 
@@ -255,7 +257,6 @@ export function TestSimpleHashReplicator() {
         console.log("performRandomOperations befor : " + beforStr);
         console.log("performRandomOperations after : " + Array.from(source).join(', '));
         console.log("perform end ================================================");
-
     }
 
     function performTest(
@@ -305,6 +306,224 @@ export function TestSimpleHashReplicator() {
             console.log(`performTest: version2 = ${version2}, endVersion2 = ${i + 1}************************`);
             performTest(source, target2, replicator, targetReplicator2, version2, i + 1);
             version2 = i + 1;
+        }
+    }
+}
+
+export class HashReplicator<T> implements IReplicator {
+    private lastVersion: number = 0;
+    private lastCheckVersion: number = 0;
+    private clone: Map<SimpleType, [IReplicator, number]>;
+    private target: Map<SimpleType, T>;
+    private actionSequence: HashSimpleVersionInfo[] = [];
+    private ctor: Consturctor<T>;
+
+    constructor(target: Map<SimpleType, T>, mark?: ReplicateMark) {
+        this.target = target;
+        let objMark = mark?.getObjMark();
+        if (objMark?.Constructor) {
+            this.ctor = objMark?.Constructor;
+        } else {
+            // 从target中获取第一个元素的构造函数
+            let item = target.values().next();
+            this.ctor = getConsturctor(item.value);
+        }
+        this.clone = new Map();
+        this.makeUpCloneData(mark);
+    }
+
+    getTarget() {
+        return this.target;
+    }
+
+    setTarget(target: any): void {
+        this.target = target;
+    }
+
+    makeUpCloneData(mark?: ReplicateMark) {
+        for (let [key, value] of this.target) {
+            this.insertData(key, value, this.lastVersion, mark);
+        }
+    }
+
+    insertData(key: SimpleType, data: T, version: number, mark?: ReplicateMark) {
+        let replicator = createReplicator(data, mark);
+        if (replicator) {
+            this.clone.set(key, [replicator, version]);
+        } else {
+            console.error("HashReplicator insertData error, data:", data);
+        }
+    }
+
+    /**
+     * 生成序列操作
+     */
+    genSequenceAction(currentVersion: number): any[] {
+        let ret: any[] = [];
+        if (this.target.size === 0) {
+            ret.push(HashActionType.Clear);
+            this.clone.clear();
+            return ret;
+        }
+
+        // 遍历target，找出需要添加的元素
+        let addItems: any[] = [];
+        for (let [key, value] of this.target) {
+            if (!this.clone.has(key) || this.clone.get(key) !== value) {
+                addItems.push(key);
+                this.insertData(key, value, currentVersion);
+            }
+        }
+        if (addItems.length > 0) {
+            ret.push(HashActionType.Add, addItems.length, ...addItems);
+        }
+
+        // 如果长度匹配则直接返回
+        if (this.target.size == this.clone.size) {
+            return ret;
+        }
+
+        // 遍历clone，找出需要删除的元素
+        let deleteItems: any[] = [];
+        for (let key of this.clone.keys()) {
+            if (!this.target.has(key)) {
+                deleteItems.push(key);
+                this.clone.delete(key);
+            }
+        }
+        if (deleteItems.length > 0) {
+            ret.push(HashActionType.Delete, deleteItems.length, ...deleteItems);
+        }
+        return ret;
+    }
+
+
+    /**
+     * 使用二分法查找有序的actionSequence中，actionSequence.version>=version的最小index
+     * @param version 
+     * @returns 
+     */
+    getActionIndex(version: number): number {
+        let left = 0;
+        let right = this.actionSequence.length - 1;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (this.actionSequence[mid].version == version) {
+                return mid;
+            } else if (this.actionSequence[mid].version < version) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    genDiff(fromVersion: number, toVersion: number): any {
+        if (toVersion < fromVersion) {
+            return false;
+        }
+        let needScan = this.lastCheckVersion < toVersion;
+        if (!needScan && fromVersion > this.lastVersion) {
+            return false;
+        }
+        if (needScan) {
+            let actions = this.genSequenceAction(toVersion);
+            this.lastCheckVersion = toVersion;
+            if (actions.length > 0) {
+                // 如果是清空操作
+                if (actions[0] == HashActionType.Clear) {
+                    this.actionSequence = [{
+                        version: toVersion,
+                        actions: actions
+                    }];
+                    this.lastVersion = toVersion;
+                    return actions;
+                } else {
+                    this.actionSequence.push({
+                        version: toVersion,
+                        actions: actions
+                    });
+                }
+            }
+        }
+
+        // 获取从fromVersion到最新的操作序列，从fromVersion的下一个操作开始
+        let fromIndex = 0;
+        if (fromVersion > 0) {
+            fromIndex = this.getActionIndex(fromVersion + 1);
+        }
+        let toIndex = this.actionSequence.length;
+        let ret = [];
+        for (let i = fromIndex; i < toIndex; ++i) {
+            ret.push(...this.actionSequence[i].actions);
+        }
+
+        // 
+
+        if (ret.length === 0) {
+            return false;
+        }
+        this.lastVersion = toVersion;
+        return ret;
+    }
+
+    applyDiff(diff: any): void {
+        if (!(diff instanceof Array)) {
+            return;
+        }
+
+        // diff的格式为：[MapActionType, count, key1, value1/key2, ...]
+        for (let i = 0; i < diff.length;) {
+            let actionType = diff[i++];
+            switch (actionType) {
+                case HashActionType.Add: {
+                    let count = diff[i++];
+                    for (let j = 0; j < count; ++j) {
+                        let key = diff[i++];
+                        let value = diff[i++];
+                        this.target.set(key, value);
+                    }
+                    break;
+                }
+                case HashActionType.Delete: {
+                    let count = diff[i++];
+                    for (let j = 0; j < count; ++j) {
+                        this.target.delete(diff[i++]);
+                    }
+                    break;
+                }
+                case HashActionType.Clear: {
+                    this.target.clear();
+                    break;
+                }
+            }
+        }
+    }
+
+    getVersion(): number {
+        return this.lastVersion;
+    }
+
+    /**
+     * 用于调试，检查数据是否正确
+     * 不正确则打印错误的数据
+     */
+     debugCheck() {
+        if (this.target.size !== this.clone.size) {
+            console.error("target.size != clone.size");
+            // 把target和clone的key组成数组都打印出来
+            let targetKeys = Array.from(this.target.keys());
+            let cloneKeys = Array.from(this.clone.keys());
+            console.error("targetKeys:", targetKeys);
+            console.error("cloneKeys:", cloneKeys);
+            return;
+        }
+        for (let [key, value] of this.target) {
+            if (!this.clone.has(key) || this.clone.get(key) !== value) {
+                console.error("target has key not in clone, key:", key, "value:", value);
+                return;
+            }
         }
     }
 }
